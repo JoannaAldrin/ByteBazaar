37300a5d14dda345cc30a1045b5e96382d83b352
JoannaAldrin <joannaaldrin28@gmail.com>
2026-03-14 12:51:57 +0400

    initial product data added to app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+const products = [
+  {
+    id: 1,
+    title: "Blue Widget",
+    description: "A stylish blue widget",
+    price: 19.99,
+    compareAt: 24.99,
+    stock: 5,
+    image: "assets/images/blue-widget.jpg"
+  },
+  {
+    id: 2,
+    title: "Red Widget",
+    description: "A flashy red widget",
+    price: 21.99,
+    compareAt: 29.99,
+    stock: 8,
+    image: "assets/images/red-widget.jpg"
+  },
+  {
+    id: 3,
+    title: "Green Widget",
+    description: "Eco-friendly green widget",
+    price: 15.99,
+    compareAt: null,
+    stock: 10,
+    image: "assets/images/green-widget.jpg"
+  }
+];
